d6450031360bcf6d90c69c88de88b49a61a3e7ab
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-02-17 10:48:55 -0300

    Arrumei o problema que tinha com não gerar outras filmes quando aberta o botão novamente

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
-const url = `${BASE_URL}${randomNumber()}${API_KEY}${language}`;
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", getMovie);
 
 function randomNumber() {
-  return Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 780.498);
 }
 
 async function getMovie() {
+  const url = `${BASE_URL}${randomNumber()}${API_KEY}${language}`;
   try {
     const response = await axios.get(url);
     const data = response.data;
     nomeFilme.textContent = data.title;
     capaFilme.setAttribute("src", `${IMG_URL}${data.poster_path}`);
     sinopseFilme.textContent = data.overview;
-    (capaFilme.style.display = "flex"), detalhes.classList.remove("isHidden");
+    capaFilme.style.display = "flex";
+    sinopseFilme.style.display = "flex";
   } catch (error) {
-    console.error(error),
-      (nomeFilme.textContent =
-        "Ops, hoje não é dia de assistir filme. Bora codar!"),
-      capaFilme.setAttribute("src", "assets/Poster.svg"),
-      detalhes.classList.remove("isHidden");
+    nomeFilme.textContent =
+      "Ainda não encontramos um filme clique novamente no botão";
+    capaFilme.style.display = "flex";
+    capaFilme.setAttribute("src", "assets/Poster.svg");
+    sinopseFilme.style.display = "none";
   }
 }
