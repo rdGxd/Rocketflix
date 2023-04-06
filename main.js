5e298f14068ec14fe40c7cd001e441349c1afa04
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-04-06 14:46:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ async function getMovie() {
   const url = `${BASE_URL}${randomNumber()}${API_KEY}${language}`;
   try {
     const response = await axios.get(url);
-    const data = response.data;
+    const data = await response.data;
     nomeFilme.textContent = data.title;
     capaFilme.setAttribute("src", `${IMG_URL}${data.poster_path}`);
     sinopseFilme.textContent = data.overview;
     capaFilme.style.display = "flex";
     sinopseFilme.style.display = "flex";
   } catch (e) {
-    console.error(e)
+    console.error(e);
     nomeFilme.textContent =
       "Ainda não encontramos um filme clique novamente no botão";
     capaFilme.style.display = "flex";
